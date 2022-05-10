6bf2fd69f7eb8c948102882b01cea806b093e9a1
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-10 19:47:24 +0600

    info card and banner added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Banner />
+            <Info />
         </div>
     );
 };
---
+++ src/pages/Home/Info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clockImg from '../../assets/icons/clock.svg'
+import markerImg from '../../assets/icons/marker.svg'
+import phoneImg from '../../assets/icons/phone.svg'
+
+const Info = () => {
+    return (
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard title='Opening Hours' bgClass=' bg-gradient-to-r from-secondary to-primary' img={clockImg} />
+            <InfoCard title='Our Locations' bgClass='bg-accent' img={markerImg} />
+            <InfoCard title='Contact Us' bgClass=' bg-gradient-to-r from-secondary to-primary' img={phoneImg} />
+        </div>
+    );
+};
+
+export default Info;
